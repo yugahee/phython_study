24dc972cdfc2b6ec87483113499747ce3424d857
유가희 <88192295+yugahee@users.noreply.github.com>
2025-07-19 15:37:38 +0900

    add : 250719 문제풀이

---
+++ study/level0/33.문자안에문자열.py
@@ -0,0 +1,12 @@
+# 문자열 str1, str2가 매개변수로 주어집니다. 
+# str1 안에 str2가 있다면 1을 없다면 2를 return하도록 solution 함수를 완성해주세요.
+
+def solution(str1, str2):
+    answer = 0
+    
+    if str1.find(str2) > -1:
+        answer = 1
+    else:
+        answer = 2
+    
+    return answer
