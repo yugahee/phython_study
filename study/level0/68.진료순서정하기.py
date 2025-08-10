379265f192c1c95712d3f732b7544aba78e9f048
유가희 <88192295+yugahee@users.noreply.github.com>
2025-08-10 16:52:47 +0900

    add : 250810 문제풀이

---
+++ study/level0/68.진료순서정하기.py
@@ -0,0 +1,13 @@
+# 외과의사 머쓱이는 응급실에 온 환자의 응급도를 기준으로 진료 순서를 정하려고 합니다. 
+# 정수 배열 emergency가 매개변수로 주어질 때 응급도가 높은 순서대로 진료 순서를 정한 배열을 return하도록 solution 함수를 완성해주세요.
+
+
+def solution(emergency):
+    answer = []
+    
+    align = sorted(emergency, reverse=True)
+    
+    for e in emergency:
+        answer.append(align.index(e)+1)
+            
+    return answer
