87895021275cafb9e8d6deadcbaff571d26c052f
유가희 <88192295+yugahee@users.noreply.github.com>
2025-08-14 12:41:04 +0900

    add : 250813 문제풀이

---
+++ study/level0/72.컨트롤제트.py
@@ -0,0 +1,18 @@
+# 숫자와 "Z"가 공백으로 구분되어 담긴 문자열이 주어집니다. 
+# 문자열에 있는 숫자를 차례대로 더하려고 합니다. 이 때 "Z"가 나오면 바로 전에 더했던 숫자를 뺀다는 뜻입니다. 
+# 숫자와 "Z"로 이루어진 문자열 s가 주어질 때, 머쓱이가 구한 값을 return 하도록 solution 함수를 완성해보세요.
+
+def solution(s):
+    answer = 0
+    
+    arr = s.split(" ")
+    
+    postNum = 0
+    for str in arr:
+        if str != "Z":
+            answer += int(str)
+            postNum = int(str)
+        else:
+            answer -= int(postNum)
+    
+    return answer
